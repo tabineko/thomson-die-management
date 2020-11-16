27b62db8c9e6a46674187ab82ed7a782f79176be
masa <jiyuujinn.mu11@gmail.com>
2020-11-16 13:48:34 +0900

    add a testcode

---
+++ test/testgui.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+
+
+class FiledialogSampleApp(ttk.Frame):
+    def __init__(self, app):
+        super().__init__(app)
+        self.pack()
+
+        self.filename = StringVar()
+
+        label = ttk.Label(self, text="FileName")
+        label.pack(side='left')
+
+        filenameEntry = ttk.Entry(self, text='', textvariable=self.filename)
+        filenameEntry.pack(side='left')
+
+        botton = ttk.Button(self, text='open', command=self.openfileDialog)
+        botton.pack(side='left')
+
+    def openfileDialog():
